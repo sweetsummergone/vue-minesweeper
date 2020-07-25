edbcf0582b4f857fe5e9bbd31393396bdaba1d9c
jessey <jessey@machine-573g.wazzup.bro>
2020-07-25 14:28:44 +0300

    view

--- src/js/minesweeper.js
+++ src/js/minesweeper.js
@@ -5,16 +5,15 @@ export class MineField {
         let m = mines || 10;
         let mineFieldArray = new Array(r*c);
         mineFieldArray.fill(0);
-        for (let i = 0; i < m; i++) {
-            mineFieldArray[i] = 'x';
-        }
+        mineFieldArray.fill('x',0,m);
+        console.log(mineFieldArray);
         this.shuffle(mineFieldArray);
         mineFieldArray = this.createField(mineFieldArray);
-        mineFieldArray = this.checkNeighbor(mineFieldArray);
+        
         return mineFieldArray;
     }
 
-    static checkNeighbor(field) { // КРАЯ НЕ ПОДСЧИТЫВАЕТ
+    static checkNeighbor(field) {
         for (let n = 1; n < field[0].length-1; n++) {
             for (let m = 1; m < field.length-1; m++) {
                 if(field[n][m] != 'x') {
@@ -62,29 +61,6 @@ export class MineField {
     static delEmptyRows(arr) {
         return 0;
     }
-    // static checkNeighbor(field) {
-    //     for (let n = 1; n < field[0]-1; n++) {
-    //         for (let m = 1; m < field.length-1; m++) {
-                
-    //             // cell_1 cell_2 cell_3
-    //             // cell_4 cell_5 cell_6
-    //             // cell_7 cell_8 cell_9
-
-    //             let cell_1 = field[n-1][m-1];
-    //             let cell_2 = field[n][m-1];
-    //             let cell_3 = field[n+1][m-1]
-
-    //             let cell_4 = field[n-1][m]
-    //             let cell_5 = field[n][m]
-    //             let cell_6 = field[n+1][m]
-
-    //             let cell_7 = field[n-1][m+1]
-    //             let cell_8 = field[n][m+1]
-    //             let cell_9 = field[n+1][m+1]
-
-    //         }
-    //     }
-    // }
 
     static shuffle(a) {
         for (let i = a.length - 1; i > 0; i--) {
@@ -98,8 +74,25 @@ export class MineField {
         let size = 10;
         let subarray = [];
         for (let i = 0; i < Math.ceil(a.length/size); i++){
-            subarray[i] = a.slice((i*size), (i*size) + size);
+            subarray[i] = a.slice((i*size), (i*size) + size)
+            subarray[i].unshift(0);
+            subarray[i].push(0);
         }
+        let k = subarray[0].length;
+        subarray.push(new Array(k));
+        subarray.unshift(new Array(k));
+        subarray = this.checkNeighbor(subarray);
+        subarray = this.deleteBorder(subarray);
         return subarray;
+    }
+
+    static deleteBorder(a) {
+        a.shift();
+        a.pop();
+        for (let i = 0; i < a.length; i++) {
+            a[i].shift()
+            a[i].pop()
+        }
+        return a;
     }
 }
